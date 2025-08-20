d23648e63f84a95c737526308fd688c6d87a9834
TaiaYovelaPang <taiayovela@gmail.com>
2025-08-21 01:56:27 +0800

    init commit

--- src/main/java/companio.java
+++ src/main/java/companio.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class companio {
     public static void main(String[] args) {
         String greeting = "Hello! I'm COMPANIO \n"
                 + "What can I do for you?";
 
-        String bye = "Bye. Hope to see you again soon!";
+        String bye = "Bye. Till next time!";
 
+        //Initial greeting
         printLine();
         System.out.println(greeting);
         printLine();
-        System.out.println(bye);
-        printLine();
+
+        //Getting input from user
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        //Echoing
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                printLine();
+                System.out.println(bye);
+                printLine();
+                break;
+            } else {
+                printLine();
+                System.out.println(input);
+                printLine();
+            }
+        }
+        scanner.close();
     }
 
     // To create horizontal line
